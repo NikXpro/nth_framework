1414323f50cdb66b4f285396a259fbc8038a6fc4
NikX <41104633+NikXpro@users.noreply.github.com>
2021-11-10 21:56:33 +0100

    remove old createCharacter Event

--- server/lifeSystem/characterManager/sv_characterManager.lua
+++ server/lifeSystem/characterManager/sv_characterManager.lua
@@ -35,18 +35,6 @@ AddEventHandler('nth:editCharacter', function(type, playerData)
     end
 end)
 
-RegisterNetEvent('nth:createCharacter')
-AddEventHandler('nth:createCharacter', function(playerData)
-    local src_ = source
-    MySQL.Sync.execute("INSERT INTO `characters` (user, firstname, lastname, birthdate, size, nationality) VALUES (@user, @firstname, @lastname, @birthdate, @size, @nationality)", {
-        ["user"] = NTH.PlayerList[src_].userId,
-        ["firstname"] = playerData.pFirstname,
-        ["lastname"] = playerData.pLastname,
-        ["birthdate"] = playerData.pBirthdate,
-        ["size"] = playerData.pSize,
-        ["nationality"] = playerData.pNationality
-    })
-end)
 
 function getUserCreated(source, loop)
     MySQL.Async.execute("SELECT id, username FROM users WHERE license = @license", {["license"] = NTH.PlayerList[source].license}, function(userData)
